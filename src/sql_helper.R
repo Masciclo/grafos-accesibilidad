53a56b89cf2cdaa01b1eae80e15bc54f25659d88
Ignacio Hernandez <ignacio.hernandez.c@usach.cl>
2022-11-28 14:48:38 +0000

    Actualización de documentación de funciones

--- src/sql_helper.R
+++ src/sql_helper.R
@@ -3,14 +3,12 @@ library(RPostgreSQL)
 library(glue)
 library(rgdal)
 
-dsn_database = "gis"
-dsn_hostname = "172.17.0.2"
-dsn_port = 5432
-dsn_pwd = "Masciclo2022"
-dsn_uid = "masciclo"
-
 dsn = paste0("PG:dbname='",dsn_database,"' host='",dsn_hostname,"' user='",dsn_uid,"' password='",dsn_pwd,"'")
 
+#' Prueba y conecta con la base de datos entregando como resultado una variable de conexión
+#' @param db Nombre de la base de datos 
+#' @param hot i
+#' @param conn Variable de conexión
 test_database_conection <- function(db, host, port, usr, pass) {
   tryCatch({
     drv <- dbDriver("PostgreSQL")
@@ -29,60 +27,296 @@ test_database_conection <- function(db, host, port, usr, pass) {
   return(connec)
 }
 
-import_shape_to_database = function(shp,db,connec,encoding) {
+
+#' Importa un shape a la base de datos
+#' @param shp Shape a importar a la base de datos
+#' @param db Nombre del shape dentro de la base de datos
+#' @param conn Variable de conexión
+import_shape_to_database = function(shp,db,conn) {
   tryCatch({
     dbWriteTable(connec,db,shp)
   })
 }
 
-update_srid = function(shp, srid, conn) {
-  dbGetQuery( conn = conn,statement = glue("SELECT UpdateGeometrySRID('{shp}','geometry',{srid});") ) 
+#' Verifica la existencia de una tabla
+#' @param table Nombre de la tabla a verificar
+#' @param conn Variable de conexión
+check_table_existence = function(table,conn) {
+  return(ifelse(
+    nrow( dbGetQuery(connec, glue("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{table}';")) ) > 0,
+    T, 
+    F))
 }
 
-change_geometry = function( shp, new_coords, old_coords, conn ) {
-  dbGetQuery(conn = conn, statement = glue ("ALTER TABLE {shp} ALTER COLUMN geometry TYPE Geometry(LINESTRING, {new_coords}) USING ST_Transform(geometry,{new_coords});"))
-}
-
+#' Crea y limpia la topología de la red
+#' @param shps Nombre de la red dentro de la base de datos
+#' @param topo_name Nombre de la topología de la red
+#' @param srid Código de sistema de coordenadas (Para el caso de Santiago corresponde a "32719")
+#' @param geometry Nombre del campo correspondiente a la geometría
+#' @param conn Variable de conexion
 create_and_clean_topology = function(shp,topo_name,srid,connec,geometry) {
-  SQL_CREATE_TOPO = glue("SELECT topology.CreateTopology('{topo_name}',{srid});")
-  dbGetQuery(connec,SQL_CREATE_TOPO)
+  init_time = Sys.time()
+  dbGetQuery(connec,glue(
+             "SELECT topology.CreateTopology('{topo_name}',{srid});
+              SELECT topology.AddTopoGeometryColumn('{topo_name}','public','{shp}','topo_geom','LINESTRING');
+              update {shp} set topo_geom = topology.toTopoGeom({geometry},'{topo_name}',1,0.001);")
+  )
+  finish_time = Sys.time()
+  print("Tiempo empleado:")
+  print(finish_time-init_time)
+}
+
+
+#' Crea los buffers que serán utilizados para cortar los inhibidores.
+#' @param lista_shps Nombre de las capas a las cuales se les aplicará el buffer
+#' @param metros Metros de buffer
+#' @param conn Variable de conexion
+create_buffer = function( lista_shps, metros = 0 , connec ) {
   
-  SQL_ADD_TOPO = glue("SELECT topology.AddTopoGeometryColumn('{topo_name}','public','{shp}','topo_geom','LINESTRING');")
-  dbGetQuery(connec,SQL_ADD_TOPO)
+  #Colapsar nombres de capas
+  collapse_shps_names = paste(lista_shps,collapse='_') # lista_shps = c(ciclo,principales) -> ciclo_principales
   
-  SQL_UPDATE_GEOM = glue("SET max_parallel_workers = 8; SET max_parallel_workers_per_gather = 4; EXPLAIN ANALYZE
-                         UPDATE {shp} SET topo_geom = topology.toTopoGeom({geometry},'{topo_name}', 1, 0.001);")
-  dbGetQuery(connec,SQL_UPDATE_GEOM)
+  #Asigna nombre del buffer segun el largo asignado
+  buffer_name = paste(collapse_shps_names,metros,sep='_') #collapse_shps = ciclo_principales, metros = 10 -> ciclos_principales_10 
+  
+  #Asigna el nombre del indice espacial
+  index_name = paste0(buffer_name,'_idx')
+  
+  #Revisa si el nombre de la tabla ya existe en la base de datos
+  table_existence = ifelse(
+    nrow( dbGetQuery(connec, glue("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{buffer_name}';")) ) > 0,
+    T, 
+    F)
+  #Si existe, no retorna nada y carga el buffer a la consulta
+  if (table_existence) {
+    print(paste0("Cargando buffer ",buffer_name))
+    create_spatial_index = dbSendQuery(connec,
+                                       glue("CREATE INDEX IF NOT EXISTS {index_name} ON {buffer_name} USING GIST (geometry);"))
+    estado = T
+    return(buffer_name)
+  }
+  #Si no existe, lo crea y le asigna un indice espacial
+  else {
+    if (metros != 0) {
+      print("Creando buffer")
+      buffer_sql =  paste(
+        lapply(
+          lista_shps,function(x) 
+            glue("select st_union(st_buffer({x}.geometry,{metros})) as geometry FROM public.{x}")),
+        collapse = ' union all ')
+      buffer_query = dbSendQuery(connec,
+                               glue("CREATE TABLE {buffer_name} AS {buffer_sql};"))
+      create_spatial_index = dbSendQuery(connec,
+                                       glue("CREATE INDEX {index_name} ON {buffer_name} USING GIST (geometry);"))
+      return(buffer_name)
+    }
+    else{
+      return(FALSE)
+    }
+  }
+}
+
+
+
+#' Corta el buffer de inhibidores con el de desinhibidores
+#' @param nombre_resultado Nombre del resultado final del proceso
+#' @param buffer_inhibidores Nombre del buffer de inhibidores
+#' @param buffer_desinhibidores Nombre del buffer de desinhibidores
+#' @param conn Variable de conexion
+buffer_difference = function(nombre_resultado,buffer_inhibidores,buffer_desinhibidores,conn) {
+  
+  if (check_table_existence("bf_{nombre_resultado}",conn)){
+    dbSendQuery(conn,glue("CREATE TABLE bf_{nombre_resultado} as
+                                  select 
+                                  st_difference(bi.geometry,bd.geometry) as geometry
+                                  from {buffer_inhibidores} bi, {buffer_desinhibidores} bd;
+                                  
+                                  create index idx_bf_{nombre_resultado}
+                                  on bf_{nombre_resultado}
+                                  using GIST(geometry);")
+    )
+  }
+  return("bf_{nombre_resultado}")
 }
 
 
 #' Corta la red según los buffer de inhibidores y/o desinhibidores.
 #' @param nombre_resultado Nombre de la red que se almacenará en la base de datos 
 #' @param red Red que será cortada por los buffers.
-#' @param id_field Nombre de columna con id unico para cada geometria.
-#' @param geom_precision Precision.
+#' @param filters Filtros que se aplicarán a la red
+#' @param lista_inh Nombre de capas que serán usadas como inhibidores.
+#' @param buffer_inh Metros de buffer para los inhibidores.
+#' @param lista_des  Nombre de capas que serán usadas como desinhibidores.
+#' @param buffer_des Metros de buffer para los desinhibidores.
+#' @param conn Variable de conexion.
 #' @return Un dataframe con las polilineas que intersectan subdivididas.
-#' @export
-#' @examples
-#' split_polylines(bicycle_network_gdf, id_field='id_2')
-cut_intermodal_network = function(nombre_resultado ,red, lista_inh,buffer_inh, lista_des,buffer_des,conn) {
+cut_intermodal_network = function(nombre_resultado, red, filters = c(), lista_inh = c(),buffer_inh = 0,lista_des = c(),buffer_des = 0,conn) {
   
+  init_time = Sys.time()
   
   #Crear buffer de inhibidores
-  buffer_inhibidores =  paste(lapply(lista_inh,function(x) glue("select st_union(st_buffer({x}.geometry,{buffer_inh})) as geometry FROM public.{x}")), collapse = ' union all ')
-  buffer_desinhibidores = paste(lapply(lista_des,function(x) glue("select st_union(st_buffer({x}.geometry,{buffer_des})) as geometry FROM public.{x}")), collapse = ' union all ')
-  finalQuery = glue("create table '{nombre_resultado}' with
-                    buffer_i as {buffer_inhibidores},
-                    buffer_d as {buffer_desinhibidores},
-                    buffer_final as (select st_difference(buffer_i.geometry, buffer_d.geometry)
-                    as geometry from buffer_i, buffer_d)
-                    select 
-                     r.o_op_ci,
-                     r.o_op_cr,
-                     r.proyect,
-                     st_difference(r.geometry, bf.geometry) as geometry
-                    from {red} r, buffer_final bf;")
+  buffer_inhibidores = create_buffer(lista_inh,buffer_inh,conn)
+  #Crear buffer de desinhibidores
+  buffer_desinhibidores = create_buffer(lista_des,buffer_des,conn)
+  #Crear buffer final
+  print("Cortando buffer de inhibidores")
+  buffer_final = buffer_difference(nombre_resultado,buffer_inhibidores,buffer_desinhibidores,conn)
+  
+  print("Cortando la red")
+  
+  if (is.null(filters)) {
+    finalQuery = glue("create table {nombre_resultado} as 
+                    select
+                     rcc.\"OBJECTID_1\",
+ 	                   rcc.\"COMUNA\",
+ 	                   rcc.\"CI_O_CR\",
+ 	                   rcc.\"TIP_VIA_CI\",
+ 	                   rcc.\"ANCHO_V\",
+ 	                   rcc.\"T_SEG_VD\",
+ 	                   rcc.\"T_SEG_CA\",
+ 	                   rcc.\"CI_VD\",
+ 	                   rcc.\"CI_PAR\",
+ 	                   rcc.\"CI_S_PAR\",
+ 	                   rcc.\"CI_CA\",
+ 	                   rcc.\"OTROS_CI\",
+ 	                   rcc.\"MATERIAL\",
+ 	                   rcc.\"VEG\",
+ 	                   rcc.\"T_VIA_CR\",
+ 	                   rcc.\"SEÑALIZAD\",
+ 	                   rcc.\"PINTADO\",
+ 	                   rcc.\"SEMAFORO\",
+ 	                   rcc.\"CARTEL\",
+ 	                   rcc.\"OTROS_CR\",
+ 	                   rcc.\"CICLOV\",
+ 	                   rcc.\"DISCON\",
+ 	                   rcc.\"LINEAS_P\",
+ 	                   rcc.\"COLOR_P\",
+ 	                   rcc.\"ANCHO_S\",
+ 	                   rcc.\"CI_PLAT\",
+ 	                   rcc.\"CI_BAND\",
+ 	                   rcc.\"PHANTO\",
+ 	                   rcc.\"COMP\",
+ 	                   rcc.largo_m,
+ 	                   rcc.\"Shape_Leng\",
+ 	                   rcc.\"DISCON_CR\",
+ 	                   rcc.\"ONEWAY\",
+ 	                   rcc.\"Id\",
+ 	                   rcc.\"CICLOVIA_N\",
+ 	                   rcc.\"PISTAS_VIA\",
+ 	                   rcc.\"ANCHO_VIA\",
+ 	                   rcc.\"TIPCI\",
+ 	                   rcc.\"FECHA_CI\",
+ 	                   rcc.\"ID_2\",
+ 	                   rcc.proyect,
+ 	                   rcc.o_op_ci,
+ 	                   rcc.o_op_cr,
+ 	                   rcc.tipologia,
+ 	                   rcc.\"type\",
+ 	                   rcc.\"name\",
+ 	                   rcc.oneway,
+ 	                   rcc.\"V_PRIV\",
+ 	                   rcc.\"T_PRIV\",
+ 	                   rcc.\"T_CAM\",
+ 	                   rcc.\"V_CAM\",
+ 	                   rcc.\"Shape_Le_1\",
+ 	                   rcc.\"T_BICI\",
+ 	                   rcc.ciclo_calle,
+                     st_difference(rcc.geometry, bf.geometry) as geometry
+                    from {red} rcc, bf_{nombre_resultado} bf;
+                    
+                    create index idx_{nombre_resultado}
+                    on {nombre_resultado}
+                    using GIST(geometry);")
+  }
+  else {
+    filter_stament = paste(filters, collapse = ' and ')
+    finalQuery = glue("create table {nombre_resultado} as 
+                    select
+                     rcc.\"OBJECTID_1\",
+ 	                   rcc.\"COMUNA\",
+ 	                   rcc.\"CI_O_CR\",
+ 	                   rcc.\"TIP_VIA_CI\",
+ 	                   rcc.\"ANCHO_V\",
+ 	                   rcc.\"T_SEG_VD\",
+ 	                   rcc.\"T_SEG_CA\",
+ 	                   rcc.\"CI_VD\",
+ 	                   rcc.\"CI_PAR\",
+ 	                   rcc.\"CI_S_PAR\",
+ 	                   rcc.\"CI_CA\",
+ 	                   rcc.\"OTROS_CI\",
+ 	                   rcc.\"MATERIAL\",
+ 	                   rcc.\"VEG\",
+ 	                   rcc.\"T_VIA_CR\",
+ 	                   rcc.\"SEÑALIZAD\",
+ 	                   rcc.\"PINTADO\",
+ 	                   rcc.\"SEMAFORO\",
+ 	                   rcc.\"CARTEL\",
+ 	                   rcc.\"OTROS_CR\",
+ 	                   rcc.\"CICLOV\",
+ 	                   rcc.\"DISCON\",
+ 	                   rcc.\"LINEAS_P\",
+ 	                   rcc.\"COLOR_P\",
+ 	                   rcc.\"ANCHO_S\",
+ 	                   rcc.\"CI_PLAT\",
+ 	                   rcc.\"CI_BAND\",
+ 	                   rcc.\"PHANTO\",
+ 	                   rcc.\"COMP\",
+ 	                   rcc.largo_m,
+ 	                   rcc.\"Shape_Leng\",
+ 	                   rcc.\"DISCON_CR\",
+ 	                   rcc.\"ONEWAY\",
+ 	                   rcc.\"Id\",
+ 	                   rcc.\"CICLOVIA_N\",
+ 	                   rcc.\"PISTAS_VIA\",
+ 	                   rcc.\"ANCHO_VIA\",
+ 	                   rcc.\"TIPCI\",
+ 	                   rcc.\"FECHA_CI\",
+ 	                   rcc.\"ID_2\",
+ 	                   rcc.proyect,
+ 	                   rcc.o_op_ci,
+ 	                   rcc.o_op_cr,
+ 	                   rcc.tipologia,
+ 	                   rcc.\"type\",
+ 	                   rcc.\"name\",
+ 	                   rcc.oneway,
+ 	                   rcc.\"V_PRIV\",
+ 	                   rcc.\"T_PRIV\",
+ 	                   rcc.\"T_CAM\",
+ 	                   rcc.\"V_CAM\",
+ 	                   rcc.\"Shape_Le_1\",
+ 	                   rcc.\"T_BICI\",
+ 	                   rcc.ciclo_calle,
+                     st_difference(rcc.geometry, bf.geometry) as geometry
+                    from {red} rcc, bf_{nombre_resultado} bf
+                    where {filter_stament};
+                    
+                    create index idx_{nombre_resultado}
+                    on {nombre_resultado}
+                    using GIST(geometry);")
+  }
+
+  
   return(dbSendQuery(conn,finalQuery))
+  
+  finish_time = Sys.time()
+  print("Tiempo empleado:")
+  print(finish_time-init_time)
 }
 
-cut_intermodal_network(lista_inh = c('buses'),buffer_inh = 10)
+#' Actualiza el SRID una vez modificada las coordenadas de geometría
+#' @param shp Nombre del shape al cual se le modificara el SRID
+#' @param srid Código SRID nuevo
+#' @param conn Variable de conexión 
+update_srid = function(shp, srid, conn) {
+  dbGetQuery( conn = conn,statement = glue("SELECT UpdateGeometrySRID('{shp}','geometry',{srid});") ) 
+}
+
+#' Cambia el sistema de coordenadas de una red
+#' @param shp Nombre de la red al cual se le modificaran las coordenadas
+#' @param new_coords Sistema de coordenadas nuevo
+#' @param old_coords Sistema de coordenadas nuevo
+#' @param conn Variable de conexión
+change_geometry = function( shp, new_coords, old_coords, conn ) {
+  dbGetQuery(conn = conn, statement = glue ("ALTER TABLE {shp} ALTER COLUMN geometry TYPE Geometry(LINESTRING, {new_coords}) USING ST_Transform(geometry,{new_coords});"))
+}
+
