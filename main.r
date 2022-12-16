e8da0d477b5460627567560e4b2db39b9c00ec2f
Ignacio Hernandez <ignacio.hernandez.c@usach.cl>
2022-12-16 14:40:14 +0000

    Incorpora crear red intermodal

--- main.r
+++ main.r
@@ -4,6 +4,7 @@ source(file = here('config.R'))
 
 connec = test_database_conection(dsn_database,dsn_hostname,dsn_port,dsn_uid,dsn_pwd)
 
+source(file = here('create_intermodal_network.R'))
 source(file = here('cut_intermodal_network.R'))
 source(file = here('calculate_component.r'))
 source(file = here('to_h3.r'))
