98399ecd7881b013d2a17f67e4f05bb033693f3c
Ignacio Hernandez <ignacio.hernandez.c@usach.cl>
2022-12-27 20:14:57 +0000

    Actualización

--- to_h3.r
+++ to_h3.r
@@ -1,12 +1,13 @@
 
-source(file = here("src/sql_helper.R"))
-source(file = here("config.R"))
-source(file = here("src/h3_helpers.R"))
+source(file = here("src/sql_helper.r"))
+source(file = here("config.r"))
+source(file = here("src/h3_helpers.r"))
 
-for (setting in setting_list) {
+for (setting in settings_list) {
   to_h3(
     h_schema = H_SCHEMA,
     h = HEX_NAME,
     x_schema = 'public',
-    x = setting$nombre_resultado)
+    x = setting$nombre_resultado,
+    connec = connec)
 }
