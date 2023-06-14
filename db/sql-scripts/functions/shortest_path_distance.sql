f00d732acc1de7951fc561d5653779c65b46c8e9
ihcs <ihernandez@mt2.cl>
2023-06-14 18:17:05 -0400

    New function

--- db/sql-scripts/functions/shortest_path_distance.sql
+++ db/sql-scripts/functions/shortest_path_distance.sql
@@ -1,16 +1,18 @@
-CREATE OR REPLACE FUNCTION shortest_path_distance(_source INTEGER, _target INTEGER)
+CREATE OR REPLACE FUNCTION shortest_path_distance(_network_table TEXT, _source INTEGER, _target INTEGER, _directed BOOLEAN)
 RETURNS DOUBLE PRECISION AS $$
 DECLARE
     result DOUBLE PRECISION;
 BEGIN
-    SELECT SUM(pt_agg.agg_cost)
-    INTO result
-    FROM pgr_dijkstra(
-        'SELECT id, source, target, length AS cost FROM my_network_table',
-        _source,
-        _target,
-        directed := false
-    ) AS pt_agg;
+    EXECUTE format(
+        'SELECT SUM(pt_agg.agg_cost)
+        FROM pgr_dijkstra(
+            ''SELECT id, source, target, length AS cost FROM %I'',
+            $1,
+            $2,
+            directed := $3
+        ) AS pt_agg',
+        _network_table
+    ) INTO result USING _source, _target, _directed;
 
     RETURN result;
 END;
