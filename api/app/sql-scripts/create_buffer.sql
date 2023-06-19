14f6310fe61322336ffd9ae7fca8aac5c28f3202
ihcs <ihernandez@mt2.cl>
2023-06-19 14:18:48 -0400

    new sql

--- api/app/sql-scripts/create_buffer.sql
+++ api/app/sql-scripts/create_buffer.sql
@@ -5,17 +5,17 @@ CREATE SCHEMA IF NOT EXISTS buffers;
 DO
 $$
 BEGIN
-    IF NOT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'buffers' AND tablename = '{buffer_name}') THEN
+    IF NOT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'buffers' AND tablename = '{result_name}') THEN
         -- Si metros no es igual a 0, crear la tabla de buffers y su correspondiente índice
         IF {metros} != 0 THEN
-            CREATE TABLE buffers.{buffer_name} AS (
-                {buffer_sql}
+            CREATE TABLE buffers.{result_name} AS (
+                SELECT ST_BUFFER(geometry,{metros}) as geometry FROM {table_name}
             );
-            CREATE INDEX {index_name} ON buffers.{buffer_name} USING GIST (geometry);
+            CREATE INDEX {result_name}_geom_idx ON buffers.{result_name} USING GIST (geometry);
         END IF;
     ELSE
         -- Si la tabla ya existe, crea el índice si no existe
-        CREATE INDEX IF NOT EXISTS {index_name} ON buffers.{buffer_name} USING GIST (geometry);
+        CREATE INDEX IF NOT EXISTS {result_name}_geom_idx ON buffers.{result_name} USING GIST (geometry);
     END IF;
 END
 $$;
