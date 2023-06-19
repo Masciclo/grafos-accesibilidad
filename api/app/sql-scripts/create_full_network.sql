60ae16f1c87584dee9c2c720d0935c50869c3051
ihcs <ihernandez@mt2.cl>
2023-06-19 14:18:30 -0400

    new sql with union all

--- api/app/sql-scripts/create_full_network.sql
+++ api/app/sql-scripts/create_full_network.sql
@@ -2,5 +2,4 @@ DROP TABLE IF EXISTS {result_name};
 
 CREATE TABLE {result_name} AS 
 SELECT ST_Union(a.geometry, b.geometry) as union_geometry 
-FROM {ciclo} AS a, {osm} AS b 
-WHERE ST_Intersects(a.geometry, b.geometry);
+FROM {ciclo} AS a, {osm} AS b;
