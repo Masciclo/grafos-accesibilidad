c82b955474b6ff4b1e7dadbdaf28ae1ab0b422ff
ihcs <ihernandez@mt2.cl>
2023-06-20 15:05:22 -0400

    main with create topology and calculate components

--- api/app/main.py
+++ api/app/main.py
@@ -12,7 +12,7 @@ import utils
 
 
 parser = argparse.ArgumentParser(description='Run necessary queries to create the tables with results in postgreSQL')
-parser.add_argument("--inhibidores", dest="inhib", required=True, type=int, help="inhibir o no la red")
+parser.add_argument("--inhibidores", dest="inhib", required=False, type=str, help="inhibir o no la red")
 parser.add_argument("--desinhibidores", dest="desinhib", required=True, type=int, help="desinhibir o no la red")
 parser.add_argument("--ciclos_path", dest="ciclos_path", required=False, type=str, help="ciclos network path")
 parser.add_argument("--osm_path", dest="osm_path", required=False, type=str, help="osm network path")
@@ -55,33 +55,69 @@ def data_pipeline(ciclo_file_path, osm_file_path, inhibitor, desinhibitor, locat
                                           'ejes_osm_cliped.geojson')
     else:
         osm_file_path_ = osm_file_path
-
+    if inhibitor is None:
+        inhibitor = os.path.join(data_base_path,
+                                 'semaforos.geojson')
 
     # Read CSV to DataFrame
     df_osm = utils.read_csv_to_df(ciclo_file_path_)
     df_ciclos = utils.read_csv_to_df(osm_file_path_)
+    df_inhibitor = utils.read_csv_to_df(inhibitor)
 
     # Create name of tables in db
     osm_table_name = f'{location}_osm'
     ciclos_table_name = f'{location}_ciclos'
+    inhibitor_table_name = f'{location}_inhibitor'
 
     # Insert DataFrame into PostgreSQL
     utils.df_to_postgres(df_osm, 
                         osm_table_name,
+                        'MULTILINESTRING',
                         USER,
                         PASSWORD,
                         HOST,
                         PORT,
                         DATABASE_NAME)
 
+    #Create spatial Index 
+    sql_file_path = os.path.join(sql_base_path,
+                                'create_spatial_index.sql')
+    query_template = utils.read_sql_file(sql_file_path)
+    query = query_template.format(layer_name=osm_table_name, 
+                                  schema_name='public')
+    
     utils.df_to_postgres(df_ciclos, 
                         ciclos_table_name,
+                        'MULTILINESTRING',
                         USER,
                         PASSWORD,
                         HOST,
                         PORT,
                         DATABASE_NAME)
+    
+    #Create spatial Index
+    sql_file_path = os.path.join(sql_base_path,
+                                'create_spatial_index.sql')
+    query_template = utils.read_sql_file(sql_file_path)
+    query = query_template.format(layer_name=ciclos_table_name, 
+                                  schema_name='public')
 
+    utils.df_to_postgres(df_inhibitor,
+                         inhibitor_table_name,
+                         'POINT',
+                         USER,
+                         PASSWORD,
+                         HOST,
+                         PORT,
+                         DATABASE_NAME)
+    
+    sql_file_path = os.path.join(sql_base_path,
+                                'create_spatial_index.sql')
+    query_template = utils.read_sql_file(sql_file_path)
+    query = query_template.format(layer_name=inhibitor_table_name, 
+                                  schema_name='public')
+
+    
     # Merge ciclo and vial network
     full_network_name = f'{location}_full_network'
     
@@ -99,21 +135,31 @@ def data_pipeline(ciclo_file_path, osm_file_path, inhibitor, desinhibitor, locat
     conn = utils.create_conn(DATABASE_NAME,HOST,PORT,USER,PASSWORD)
 
     # Execute query to create full network
+    print('Creando red intermodal')
     params = ()
     utils.execute_query_with_params(conn, query, params)
+    
+    # Create Spatial index for the full network
+    sql_file_path = os.path.join(sql_base_path,
+                                'create_spatial_index.sql')
+    query_template = utils.read_sql_file(sql_file_path)
+    query = query_template.format(layer_name=full_network_name, 
+                                  schema_name='public')
 
+
+    """
     # Use Inhibitor or not
     if inhibitor:
         # Define path to sql query and table names
         sql_file_path_inhib = os.path.join(sql_base_path,
                                 'create_buffer.sql')
-        inhibitor_input_name = f'{location}_inhib'
+        inhibitor_input_name = inhibitor_table_name
         inhibitor_result_name = f'{location}_network_inhib'
         query_template_inhib = utils.read_sql_file(sql_file_path_inhib)
         # Format sql query
         query = query_template_inhib.format(result_name=inhibitor_result_name, 
-                                  inhibitor_table_name=inhibitor_input_name, 
-                                  metros=osm_table_name
+                                  table_name=inhibitor_input_name, 
+                                  metros=50
                                   # agregar todos los,
                                   # parametros de query
                                   ) 
@@ -151,15 +197,45 @@ def data_pipeline(ciclo_file_path, osm_file_path, inhibitor, desinhibitor, locat
         utils.execute_query_with_params(conn, query, params)
     else:
         print('desinhibitor are not processing')
-
+    """
 
     # Final difference
     
     # delete_line_segments_in_polygon
 
-    # create_clean_topology
+    # Create and clean the topology
 
-    # calculate things
+    topology_table_name = full_network_name+'_topo'
+    # Read SQL file and format query string
+    sql_file_path = os.path.join(sql_base_path,
+                                'create_clean_topology.sql')
+
+    # Read template query and add parameters                                
+    query_template = utils.read_sql_file(sql_file_path)
+    query = query_template.format(topo=topology_table_name,
+                                  srid=32719,
+                                  table=full_network_name)
+    print('Generando Topología')                      
+    params = ()
+    utils.execute_query_with_params(conn, query, params)
+
+    # Calculate Components
+    sql_file_path = os.path.join(sql_base_path,
+                                'calculate_components.sql')
+
+    # Read template query and add parameters
+    components_table_name = full_network_name+'_components'                           
+    query_template = utils.read_sql_file(sql_file_path)
+    query = query_template.format(topo=topology_table_name,
+                                  components_table=components_table_name)
+    print('Calculando componentes')                      
+    params = ()
+    utils.execute_query_with_params(conn, query, params)
+
+    # Calculate Betweenness centrality
+
+    # Calculate Closeness
+    
 
 
 def main():
