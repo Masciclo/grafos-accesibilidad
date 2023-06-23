bd601e42f4b5d7d759a8a2126d272d06265ee105
ihcs <ihernandez@mt2.cl>
2023-06-22 20:12:53 -0400

    new query

--- api/app/sql-scripts/create_full_network.sql
+++ api/app/sql-scripts/create_full_network.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS {result_name};
 
 CREATE TABLE {result_name} AS 
-SELECT a.geometry as the_geom FROM {ciclo} AS a 
+SELECT a.geometry as geometry FROM {ciclo} AS a 
 UNION ALL
-SELECT b.geometry as the_geom FROM {osm} AS b;
+SELECT b.geometry as geometry FROM {osm} AS b;
+
+ALTER TABLE {result_name} ADD COLUMN id SERIAL PRIMARY KEY;
