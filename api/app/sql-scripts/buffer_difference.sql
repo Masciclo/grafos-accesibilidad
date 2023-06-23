7311bb3a604e6f1769abdd5c2fac5037364fc2b3
ihcs <ihernandez@mt2.cl>
2023-06-22 20:18:15 -0400

    new query

--- api/app/sql-scripts/buffer_difference.sql
+++ api/app/sql-scripts/buffer_difference.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS buffers.bf_{result_name} AS 
+CREATE TABLE IF NOT EXISTS buffers.{result_name} AS 
 SELECT 
     ST_Difference(bi.geometry, bd.geometry) AS geometry 
 FROM buffers.{buffer_inhibitor} bi, 
     buffers.{buffer_desinhibitor} bd; 
 
-CREATE INDEX IF NOT EXISTS idx_bf_{result_name} 
-ON buffers.bf_{result_name} USING GIST(geometry);
+CREATE INDEX IF NOT EXISTS idx_{result_name} 
+ON buffers.{result_name} USING GIST(geometry);
