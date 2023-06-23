996e65a0ac8584d3c8f4b44fc4d19149b996ad2c
ihcs <ihernandez@mt2.cl>
2023-06-22 20:18:41 -0400

    new query

--- api/app/sql-scripts/calculate_components.sql
+++ api/app/sql-scripts/calculate_components.sql
@@ -1,18 +1,25 @@
-CREATE TABLE input_connectedComponents AS
+CREATE TABLE {table_name}_results AS
 SELECT
     edge_id AS id,
     start_node AS source,
     end_node AS target,
     ST_Length(geom) AS cost,
-	geom as the_geom
+    geom as the_geom,
+    0 AS component 
 FROM
     {topo}.edge_data;
 
 -- Crear una tabla con los componentes
-DROP TABLE IF EXISTS {components_table};
-CREATE TABLE {components_table} AS
+DROP TABLE IF EXISTS components_result;
+CREATE TABLE components_result AS
 SELECT * FROM pgr_connectedComponents(
-    'SELECT id, source, target, cost FROM input_connectedComponents'
+    'SELECT id, source, target, cost FROM {table_name}_results'
 );
 
-DROP TABLE input_connectedComponents;
+-- Actualizar la tabla de red con la información de los componentes
+UPDATE {table_name}_results
+SET component = components_result.component
+FROM components_result
+WHERE {table_name}_results.source = components_result.node;
+
+DROP TABLE components_result;
